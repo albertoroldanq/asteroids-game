00a55fca28aff34b88996751d3b04203b4e38a73
alberto <albertoroldanq@gmail.com>
2024-12-24 10:30:23 +0000

    initial work to add automated tests

---
+++ tests/test_asteroid.py
@@ -0,0 +1,19 @@
+import pytest
+import pygame
+
+from asteroid import Asteroid
+
+
+@pytest.fixture
+def asteroid():
+    return Asteroid(0, 0, 10)
+
+
+def test_asteroid_split(asteroid):
+    asteroid.split()
+    assert asteroid.alive() == False
+
+
+def test_asteroid_update(asteroid):
+    asteroid.update(1)
+    assert asteroid.position == pygame.Vector2(0, 0)
