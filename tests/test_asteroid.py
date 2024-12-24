ef86d2b715811af50ad66e9371298003eb3a4745
alberto <albertoroldanq@gmail.com>
2024-12-24 12:21:43 +0000

    add warning exception to pylint config

--- tests/test_asteroid.py
+++ tests/test_asteroid.py
@@ -4,15 +4,15 @@ import pygame
 from asteroid import Asteroid
 
 @pytest.fixture
-def asteroid_fixture():
+def asteroid():
     return Asteroid(0, 0, 10)
 
 
-def test_asteroid_split(asteroid_fixture):
-    asteroid_fixture.split()
-    assert asteroid_fixture.alive() is False
+def test_asteroid_split(asteroid):
+    asteroid.split()
+    assert asteroid.alive() is False
 
 
-def test_asteroid_update(asteroid_fixture):
-    asteroid_fixture.update(1)
-    assert asteroid_fixture.position == pygame.Vector2(0, 0)
+def test_asteroid_update(asteroid):
+    asteroid.update(1)
+    assert asteroid.position == pygame.Vector2(0, 0)
